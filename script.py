509b2f6d127a92f46e14ad57727c6b1c34fab065
Rachappah <Rachappa@HP>
2017-04-05 16:37:52 +0530

    Test script

---
+++ script.py
@@ -0,0 +1,19 @@
+'''
+Created on 05/04/2017
+
+@author: Rachappa
+'''
+from selenium import webdriver
+import time
+if __name__ == '__main__':
+    driver=webdriver.Firefox()
+    driver.get("http://www.abhibus.com/")
+    time.sleep(2)
+    driver.find_element_by_class_name("abhi-modal-close").click()
+    driver.find_element_by_xpath(".//*[@id='source']").send_keys("Bidar")
+    driver.find_element_by_xpath(".//*[@id='destination']").send_keys("Bangalore")
+    driver.find_element_by_xpath(".//*[@id='datepicker1']").click()
+    driver.find_element_by_xpath("//a[text()='24']").click()
+    driver.find_element_by_xpath(".//*[@id='datepicker2']").click()
+    driver.find_element_by_xpath("//a[text()='26']").click()
+    driver.find_element_by_xpath(".//*[@id='roundTrip']/a").click()
